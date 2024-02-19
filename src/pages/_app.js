4c94869446ed051c1636ed5dc5dd657381a08556
Pawan <pawanbajaj2012@gmail.com>
2024-02-20 01:11:57 +0530

    frontend added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,42 @@
-import '@/styles/globals.css'
+import Header from '@/components/header';
+import '@/styles/globals.css';
+import { SessionProvider } from "next-auth/react"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+import { Toaster } from 'react-hot-toast';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { polygonMumbai } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+
+const { chains, publicClient } = configureChains([polygonMumbai], [publicProvider()]);
+const { connectors } = getDefaultWallets({
+	appName: 'deDrive',
+	projectId: '9d76ee4cef7c5b8d16e72986446c7841',
+	chains,
+});
+
+const wagmiConfig = createConfig({
+	autoConnect: true,
+	connectors,
+	publicClient,
+});
+
+export default function App({
+	Component,
+	pageProps: { session, ...pageProps },
+}) {
+	return (
+		<SessionProvider session={session}>
+			<WagmiConfig config={wagmiConfig}>
+				<RainbowKitProvider chains={chains} modalSize="compact">
+					<Header>
+						<Component {...pageProps} />
+						<Toaster />
+					</Header>
+				</RainbowKitProvider>
+			</WagmiConfig>
+		</SessionProvider>
+	);
 }
